150bc25ab8d5dc72fd28df882821ec9ff7b48718
Sumit Kumar <sk990005@gmail.com>
2021-12-17 12:22:50 +0530

    Create directory structure

--- index.js
+++ index.js
@@ -15,6 +15,27 @@ const createDirectoriesStructure = async (dirPath) => {
     const titleFilePath = join(dirPath, "titles.txt");
     const readTitleFileData = await readFile(titleFilePath);
     const titles = readTitleFileData.toString().split("\r\n");
+
+    let directorySequence = 1;
+    let newDirectoryPath = "";
+
+    titles.forEach(async (title) => {
+      if (title[0].match(/^[A-Z]/)) {
+        const modifiedTitleName = `${directorySequence++}. ${title.slice(3)}`;
+        newDirectoryPath = join(dirPath, modifiedTitleName);
+        try {
+          await mkdir(newDirectoryPath, { recursive: true });
+        } catch (err) {
+          console.log(`Directory Error: ${err.message}`);
+        }
+      } else {
+        try {
+          await writeFile(join(newDirectoryPath, `${title}.txt`), "");
+        } catch (err) {
+          console.log(`File Error: ${err.message}`);
+        }
+      }
+    });
   } catch (err) {
     console.log(`Error: ${err.message}`);
   }
